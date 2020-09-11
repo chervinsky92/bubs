bf04c2ef3cda554ec4bce83291acf783b81ce3b0
chervinsky92 <appdesigner2192@gmail.com>
2020-09-11 15:32:47 -0400

    Close navbar script

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/* Script 1: 
+Close nav menu after clicking on link (small screens)
+
+.navbar-collapse is clicked on while open ->
+.navbar-toggler will be toggled ->
+closing .navbar-collapse ->
+while going to the section corresponding to the clicked link */
+
+$(document).ready(function() {
+    $(document).click(function(event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
